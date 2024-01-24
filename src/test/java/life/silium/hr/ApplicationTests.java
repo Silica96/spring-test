f13d1713ad3a9175dc3c89f410083b4785252d40
Gyuseok Jung <gsjung@whatap.io>
2024-01-24 17:06:11 +0900

    init

---
+++ src/test/java/life/silium/hr/ApplicationTests.java
@@ -0,0 +1,13 @@
+package life.silium.hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
